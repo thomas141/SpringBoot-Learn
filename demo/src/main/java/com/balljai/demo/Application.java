0a78b67e174c126e38aa18a82c87c874e449089c
Thomas Lee <tkleeap@connect.ust.hk>
2025-02-02 15:05:22 +0800

    update

--- demo/src/main/java/com/balljai/demo/Application.java
+++ demo/src/main/java/com/balljai/demo/Application.java
@@ -1,14 +1,22 @@
 package com.balljai.demo;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
+	public static void main(String[] args) {
+		//what will be run here is the Spring Boot Application
 		SpringApplication.run(Application.class, args);
+
+		log.info("Application started successfully!");
 	}
 
 }
